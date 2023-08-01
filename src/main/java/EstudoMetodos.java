dc236847a25af4e9dd3508ca9a122485470475a9
Lucas Takamoto <lucas.takamoto@hotmail.com>
2023-08-01 09:11:36 -0300

    Java-Metodos

--- src/main/java/EstudoMetodos.java
+++ src/main/java/EstudoMetodos.java
@@ -1,27 +1,37 @@
-class Garrafa(){
+class Garrafa {
     float volume;
 
-    public float obterVolume(){
+    public float obterVolume() {
         return this.volume;
     }
 }
 
 public class EstudoMetodos {
-    public String bemVindo(String nome){
-        return "Bem-vindo," + nome;
+    // Métodos/Funções -> São estruturas executam uma determinada terafa/atividade
+
+    // Funções Que Recebem Parâmetros e Quem Não Recebem
+    // Funções Que Retorna Valores e Quem Não Retornam
+    public String bemVindo(String nome) {
+        return "Bem vindo, " + nome;
     }
 
-    public void imprimir(String texto){
+    public void imprimir(String texto) {
         System.out.println(texto);
     }
 
-    public static double soma (double n1, double n2, double n3){
+    public static double soma(double n1, double n2) {
+        double s = n1 + n2;
+        return s;
+    }
+
+    public static double soma(double n1, double n2, double n3) {
         double s = n1 + n2 + n3;
         return s;
     }
 
     public static void main(String[] args) {
-        double resultado = soma(12.3, 34.3, 7.4);
+        double resultado = soma(12.1, 34.3, 7.4);
+        resultado = soma(12.1, 34.3);
 //        System.out.println(resultado);
 
         Garrafa garrafa = new Garrafa();
