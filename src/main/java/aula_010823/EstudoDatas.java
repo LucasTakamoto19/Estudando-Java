1bef4845f09b794f72302db6902aa91b7fc0b2b4
Lucas Takamoto <lucas.takamoto@hotmail.com>
2023-08-02 15:12:09 -0300

    Aula-Listas e Datas

--- src/main/java/aula_010823/EstudoDatas.java
+++ src/main/java/aula_010823/EstudoDatas.java
@@ -1,3 +1,5 @@
+package aula_010823;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
--- src/main/java/aula_010823/EstudoListas.java
+++ src/main/java/aula_010823/EstudoListas.java
@@ -1,3 +1,5 @@
+package aula_010823;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
